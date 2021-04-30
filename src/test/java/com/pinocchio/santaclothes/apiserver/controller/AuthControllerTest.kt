3d38f69325826acf3210a024b9285e79f485d409
seongahjo <seongside@gmail.com>
2021-05-01 03:06:05 +0900

    controller 테스트 mocking 적용

--- src/test/java/com/pinocchio/santaclothes/apiserver/controller/AuthControllerTest.kt
+++ src/test/java/com/pinocchio/santaclothes/apiserver/controller/AuthControllerTest.kt
@@ -2,18 +2,29 @@ package com.pinocchio.santaclothes.apiserver.controller
 
 import com.pinocchio.santaclothes.apiserver.controller.dto.RegisterRequest
 import com.pinocchio.santaclothes.apiserver.entity.AccountType
+import com.pinocchio.santaclothes.apiserver.service.UserService
 import com.pinocchio.santaclothes.apiserver.test.ApiTest
 import io.restassured.http.ContentType
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.springframework.boot.test.mock.mockito.MockBean
 
 
 class AuthControllerTest : ApiTest() {
+    @MockBean
+    lateinit var userService: UserService
+
     @Test
     fun register() {
-        val registerRequest = RegisterRequest("token", "name", AccountType.KAKAO)
+        val token = "token"
+        val name = "name"
+        val accountType = AccountType.KAKAO
+
+        val registerRequest = RegisterRequest(token, name, accountType)
 
         Given {
             contentType(ContentType.JSON)
@@ -23,5 +34,7 @@ class AuthControllerTest : ApiTest() {
         } Then {
             statusCode(201)
         }
+
+        verify(userService, times(1)).register(token, name, accountType)
     }
 }
