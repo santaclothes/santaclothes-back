29985140377213a350e297678e01ae25fe642026
seongahjo <seongside@gmail.com>
2021-05-03 01:29:39 +0900

    개발시에 h2를 사용하도록 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS CLOTH
 (
-    id    bigserial PRIMARY KEY,
+    id    integer auto_increment PRIMARY KEY,
     name  varchar(255),
     color varchar(20),
     type  varchar(20)
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS CLOTH
 
 CREATE TABLE IF NOT EXISTS CARE_LABEL
 (
-    id           bigserial PRIMARY KEY,
+    id           integer auto_increment PRIMARY KEY,
     water_type   varchar(50),
     bleach_type  varchar(50),
     dry_type     varchar(50),
@@ -29,11 +29,11 @@ CREATE TABLE IF NOT EXISTS "USER"
 
 CREATE TABLE IF NOT EXISTS USER_TOKEN
 (
-    id            bigserial PRIMARY KEY,
+    id            integer auto_increment PRIMARY KEY,
     user_token    varchar(64),
     access_token  uuid unique,
     refresh_token uuid,
-    expired_at    timestamp default now() + interval '1 week',
+    expired_at    timestamp default DATEADD('DAY', 7, NOW()),
     created_at    timestamp default now(),
     FOREIGN KEY (user_token)
         REFERENCES "USER" (token)
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS USER_TOKEN
 
 CREATE TABLE IF NOT EXISTS IMAGE
 (
-    id             bigserial PRIMARY KEY,
+    id             integer auto_increment PRIMARY KEY,
     file_path      varchar(255),
     thumbnail_path varchar(255),
     image_type     varchar(50),
