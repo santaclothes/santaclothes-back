355adcfd9c5e66589e103978a86fbbbf06334d4c
hhaggang22 <20181018@sungshin.ac.kr>
2021-05-23 18:56:23 +0900

    분석결과화면 확인 페이지

---
+++ src/main/java/com/pinocchio/santaclothes/apiserver/dto/AnalyzeDto.java
@@ -0,0 +1,2 @@
+package com.pinocchio.santaclothes.apiserver.dto;public class AnalyzeDto {
+}
---
+++ src/main/java/com/pinocchio/santaclothes/apiserver/service/ImageAnalyzeServiceImpl.java
@@ -0,0 +1,15 @@
+package com.pinocchio.santaclothes.apiserver.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pinocchio.santaclothes.apiserver.dto.AnalyzeDto;
+
+@Service
+public class ImageAnalyzeService {
+
+	@Autowired
+	AnalyzeDto analyzeDto;
+
+
+}
