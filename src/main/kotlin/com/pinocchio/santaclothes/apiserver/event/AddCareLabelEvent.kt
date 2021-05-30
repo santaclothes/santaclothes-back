216ff777a2dd7799ac357f1220034890237ab766
ah.jo <seongside@gmail.com>
2021-05-31 01:21:59 +0900

    이미지와 케어라벨 종속성 제거 (#9)
    이벤트를 통해 연관관계 설정하도록 수정

---
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/event/AddCareLabelEvent.kt
@@ -0,0 +1,3 @@
+package com.pinocchio.santaclothes.apiserver.event
+
+data class AddCareLabelEvent(val careLabelId: Long, val careLabelImageId: Long)
---
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/event/handler/ClothEventHandler.kt
@@ -0,0 +1,21 @@
+package com.pinocchio.santaclothes.apiserver.event.handler
+
+import com.pinocchio.santaclothes.apiserver.event.AddCareLabelEvent
+import com.pinocchio.santaclothes.apiserver.repository.ImageRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+class ClothEventHandler(
+    @Autowired val imageRepository: ImageRepository
+) {
+
+    // @TransactionalEventListener? commit 된 후에 동작, 테스트에서는 Rollback이 이루어지기 떄문에 동작 X
+    @EventListener
+    fun addCareLabel(event: AddCareLabelEvent) {
+        val careLabelImage = imageRepository.findCareLabelById(event.careLabelImageId).orElseThrow()
+        careLabelImage.careLabelId = event.careLabelId
+        imageRepository.save(careLabelImage)
+    }
+}
