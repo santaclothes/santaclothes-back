20b35d647b40d44c36784f3e240af124e2ad6ef5
ah.jo <ah.jo@navercorp.com>
2021-07-17 04:39:40 +0900

    Api Dto 분리

--- src/main/kotlin/com/pinocchio/santaclothes/apiserver/controller/ApiController.kt
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/controller/ApiController.kt
@@ -1,10 +1,12 @@
 package com.pinocchio.santaclothes.apiserver.controller
 
-import com.pinocchio.santaclothes.apiserver.entity.AnalysisStatus
-import com.pinocchio.santaclothes.apiserver.entity.type.ClothesColor
-import com.pinocchio.santaclothes.apiserver.entity.type.ClothesType
+import com.pinocchio.santaclothes.apiserver.controller.dto.AnalysisRequestForm
+import com.pinocchio.santaclothes.apiserver.controller.dto.AnalysisRequestResult
+import com.pinocchio.santaclothes.apiserver.controller.dto.AnalysisStatusRequest
+import com.pinocchio.santaclothes.apiserver.controller.dto.NotificationElement
+import com.pinocchio.santaclothes.apiserver.controller.dto.NotificationList
+import com.pinocchio.santaclothes.apiserver.controller.dto.toAnalysisRequestDocument
 import com.pinocchio.santaclothes.apiserver.notification.service.NotificationService
-import com.pinocchio.santaclothes.apiserver.service.AnalysisRequestDocument
 import com.pinocchio.santaclothes.apiserver.service.AnalysisRequestService
 import com.pinocchio.santaclothes.apiserver.service.UserService
 import com.pinocchio.santaclothes.apiserver.support.authorizationToUuid
@@ -22,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
 @Api(tags = ["Api"])
 @RestController
@@ -75,39 +76,3 @@ class ApiController(
         return NotificationList(user.name, notificationElements)
     }
 }
-
-data class AnalysisRequestForm(
-    val clothImage: MultipartFile,
-    val labelImages: MultipartFile,
-    val clothName: String,
-    val clothType: ClothesType,
-    val clothColor: ClothesColor,
-)
-
-data class AnalysisRequestResult(
-    val analysisRequestId: Long,
-)
-
-data class AnalysisStatusRequest(
-    val status: AnalysisStatus
-)
-
-data class NotificationList(
-    val userName: String,
-    val notificationElements: List<NotificationElement>
-)
-
-data class NotificationElement(
-    val id: Long,
-    val analysisRequestId: Long,
-    val clothName: String
-)
-
-fun AnalysisRequestForm.toAnalysisRequestDocument() =
-    AnalysisRequestDocument(
-        clothName = clothName,
-        clothesColor = clothColor,
-        clothesType = clothType,
-        clothImage = clothImage,
-        labelImage = labelImages
-    )
---
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/controller/dto/ApiDto.kt
@@ -0,0 +1,43 @@
+package com.pinocchio.santaclothes.apiserver.controller.dto
+
+import com.pinocchio.santaclothes.apiserver.entity.AnalysisStatus
+import com.pinocchio.santaclothes.apiserver.entity.type.ClothesColor
+import com.pinocchio.santaclothes.apiserver.entity.type.ClothesType
+import com.pinocchio.santaclothes.apiserver.service.AnalysisRequestDocument
+import org.springframework.web.multipart.MultipartFile
+
+data class AnalysisRequestForm(
+    val clothImage: MultipartFile,
+    val labelImages: MultipartFile,
+    val clothName: String,
+    val clothType: ClothesType,
+    val clothColor: ClothesColor,
+)
+
+data class AnalysisRequestResult(
+    val analysisRequestId: Long,
+)
+
+data class AnalysisStatusRequest(
+    val status: AnalysisStatus
+)
+
+data class NotificationList(
+    val userName: String,
+    val notificationElements: List<NotificationElement>
+)
+
+data class NotificationElement(
+    val id: Long,
+    val analysisRequestId: Long,
+    val clothName: String
+)
+
+fun AnalysisRequestForm.toAnalysisRequestDocument() =
+    AnalysisRequestDocument(
+        clothName = clothName,
+        clothesColor = clothColor,
+        clothesType = clothType,
+        clothImage = clothImage,
+        labelImage = labelImages
+    )
