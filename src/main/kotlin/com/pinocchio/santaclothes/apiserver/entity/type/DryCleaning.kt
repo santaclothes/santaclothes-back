476fe7fc24cb7a53940be5d6310e28b634073565
hhaggang22 <20181018@sungshin.ac.kr>
2021-07-17 22:57:57 +0900

    entity description 자세하게 수정

--- src/main/kotlin/com/pinocchio/santaclothes/apiserver/entity/type/DryCleaning.kt
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/entity/type/DryCleaning.kt
@@ -1,30 +1,30 @@
 package com.pinocchio.santaclothes.apiserver.entity.type
 
 enum class DryCleaning(var code: Int, var nationType: NationType, var description: String, var imageUrl:String) {
-    DRY_CLEANING_KR(0, NationType.KR, "드라이 클리닝 가능", "/img/dryCleaning/can.png"), DRY_CLEANING_FORBIDDEN_KR(
+    DRY_CLEANING_KR(0, NationType.KR, "드라이 클리닝이 가능합니다", "/img/dryCleaning/can.png"), DRY_CLEANING_FORBIDDEN_KR(
         1,
         NationType.KR,
-        "드라이 클리닝 불가", "/img/dryCleaning/no.png"
+        "드라이 클리닝이 불가합니다", "/img/dryCleaning/no.png"
     ),
-    DRY_CLEANING_OIL_KR(2, NationType.KR, "석유계 드라이 클리닝 가능", "/img/dryCleaning/half.png"), DRY_CLEANING_SELF_FORBIDDEN_KR(
+    DRY_CLEANING_OIL_KR(2, NationType.KR, "석유계 드라이 클리닝이 가능합니다", "/img/dryCleaning/half.png"), DRY_CLEANING_SELF_FORBIDDEN_KR(
         3,
         NationType.KR,
-        "셀프 드라이 클리닝 불가", "/img/dryCleaning/noself.png"
+        "셀프 드라이 클리닝이 불가합니다", "/img/dryCleaning/noself.png"
     ),
-    DRY_CLEANING_USA(4, NationType.USA, "드라이 클리닝 가능", "/img/dryCleaning/ame_dryCleaning.png"), DRY_CLEANING_FORBIDDEN_USA(
+    DRY_CLEANING_USA(4, NationType.USA, "드라이 클리닝이 가능합니다", "/img/dryCleaning/ame_dryCleaning.png"), DRY_CLEANING_FORBIDDEN_USA(
         5,
         NationType.USA,
-        "드라이 클리닝 불가", "/img/dryCleaning/ame_no.png"
+        "드라이 클리닝이 불가합니다", "/img/dryCleaning/ame_no.png"
     ),
-    DRY_CLEANING_SHORT_USA(6, NationType.USA, "짧은 드라이 클리닝", "/img/dryCleaning/ame_short.png"), DRY_CLEANING_LOW_HUMIDITY_USA(
+    DRY_CLEANING_SHORT_USA(6, NationType.USA, "짧은 드라이 클리닝만 가능합니다", "/img/dryCleaning/ame_short.png"), DRY_CLEANING_LOW_HUMIDITY_USA(
         7,
         NationType.USA,
-        "낮은 습도에서 드라이 클리닝", "/img/dryCleaning/ame_lowmoi.png"
+        "낮은 습도에서 드라이 클리닝이 필요합니다", "/img/dryCleaning/ame_lowmoi.png"
     ),
-    DRY_CLEANING_NOT_HOT_USA(8, NationType.USA, "열을 줄여서 드라이 클리닝", "/img/dryCleaning/ame_lowheat.png"), DRY_CLEANING_JP(
+    DRY_CLEANING_NOT_HOT_USA(8, NationType.USA, "열을 줄여서 드라이 클리닝이 필요합니다", "/img/dryCleaning/ame_lowheat.png"), DRY_CLEANING_JP(
         9,
         NationType.JP,
-        "드라이 클리닝 가능", "/img/dryCleaning/jap_can.png"
+        "드라이 클리닝이 가능합니다", "/img/dryCleaning/jap_can.png"
     ),
-    DRY_CLEANING_FORBIDDEN_JP(10, NationType.JP, "드라이 클리닝 불가", "/img/dryCleaning/jap_no.png");
+    DRY_CLEANING_FORBIDDEN_JP(10, NationType.JP, "드라이 클리닝이 불가합니다", "/img/dryCleaning/jap_no.png");
 }
--- src/main/kotlin/com/pinocchio/santaclothes/apiserver/entity/type/WaterType.kt
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/entity/type/WaterType.kt
@@ -1,30 +1,30 @@
 package com.pinocchio.santaclothes.apiserver.entity.type
 
 enum class WaterType(var code: Int, var nationType: NationType, var description: String, var imageUrl:String) {
-    HAND_KR(0, NationType.KR, "손 세탁 가능, 중성 세제", "/img/waterwash/handwash.png"), FORBIDDEN_KR(1, NationType.KR, "물세탁 불가", "/img/waterwash/nohandwash.png"), WASHER_30_NEUTRAL_KR(
+    HAND_KR(0, NationType.KR, "물 온도 30°C로 중성세제를 사용하여 손세탁이 필요합니다.", "/img/waterwash/handwash.png"), FORBIDDEN_KR(1, NationType.KR, "물세탁이 불가합니다", "/img/waterwash/nohandwash.png"), WASHER_30_NEUTRAL_KR(
         2,
         NationType.KR,
-        "세탁기 약 30도 중성", "/img/waterwash/machine_30.png"
+        "물 온도 30°C로 중성세제를 사용하여 세탁기로 약하게 세탁하세요", "/img/waterwash/machine_30.png"
     ),
-    WASHER_ABOUT_40_KR(3, NationType.KR, "세탁기 약 40도", "/img/waterwash/machine_40.png"), WASHER_40_KR(4, NationType.KR, "세탁기 40도", "/img/waterwash/machine40.png"), WASHER_60_KR(
+    WASHER_ABOUT_40_KR(3, NationType.KR, "물 온도 40°C로 세탁기를 사용하여 약하게 세탁하세요", "/img/waterwash/machine_40.png"), WASHER_40_KR(4, NationType.KR, "물 온도 40°C로 세탁기를 사용하여 세탁하세요", "/img/waterwash/machine40.png"), WASHER_60_KR(
         5,
         NationType.KR,
-        "세탁기 60도", "/img/waterwash/machine60.png"
+        "물 온도 60°C로 세탁기를 사용하여 세탁하세요", "/img/waterwash/machine60.png"
     ),
-    WASHER_90_KR(6, NationType.KR, "세탁기 95도", "/img/waterwash/machine95.png"), HAND_USA(7, NationType.USA, "손 세탁 가능", "/img/waterwash/ame_handwash.png"), FORBIDDEN_USA(
+    WASHER_90_KR(6, NationType.KR, "물 온도 95°C로 세탁기를 사용하여 세탁하세요(삶기 가능)", "/img/waterwash/machine95.png"), HAND_USA(7, NationType.USA, "손 세탁이 가능합니다", "/img/waterwash/ame_handwash.png"), FORBIDDEN_USA(
         8,
         NationType.USA,
-        "물 세탁 금지", "/img/waterwash/ame_no.png"
+        "물 세탁이 불가합니다", "/img/waterwash/ame_no.png"
     ),
-    WASHER_USA(9, NationType.USA, "기계 세탁 가능", "/img/waterwash/ame_mac.png"), FORBIDDEN_BLOWER_USA(10, NationType.USA, "비틀기 금지", "/img/waterwash/ame_nosqueeze.png"), HAND_30_JP(
+    WASHER_USA(9, NationType.USA, "보통세기로 기계세탁하세요", "/img/waterwash/ame_mac.png"), FORBIDDEN_BLOWER_USA(10, NationType.USA, "비틀기가 불가합니다", "/img/waterwash/ame_nosqueeze.png"), HAND_30_JP(
         11,
         NationType.JP,
-        "30도 손세탁 가능", "/img/waterwash/jap_30hand.png"
+        "물 온도 30°C로 세탁기를 사용하여 세탁하세요", "/img/waterwash/jap_30hand.png"
     ),
-    WASHER_95_JP(12, NationType.JP, "세탁기 95도", "/img/waterwash/jap_95mac.png"), HAND_WEAK_JP(13, NationType.JP, "약한 손세탁", "/img/waterwash/jap_light.png"), FORBIDDEN_BLOWER_JP(
+    WASHER_95_JP(12, NationType.JP, "물 온도 95°C로 세탁기를 사용하여 세탁하세요", "/img/waterwash/jap_95mac.png"), HAND_WEAK_JP(13, NationType.JP, "약하게 손세탁하세요", "/img/waterwash/jap_light.png"), FORBIDDEN_BLOWER_JP(
         14,
         NationType.JP,
-        "비틀기 금지", "/img/waterwash/jap_nosqueeze.png"
+        "비틀기가 불가합니다", "/img/waterwash/jap_nosqueeze.png"
     ),
-    FORBIDDEN_JP(15, NationType.JP, "물세탁 불가", "/img/waterwash/jap_no.png");
+    FORBIDDEN_JP(15, NationType.JP, "물세탁이 불가합니다", "/img/waterwash/jap_no.png");
 }
