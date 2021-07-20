9a36105ea9d816fe942a5cc7e96a7a7766d52e61
seongahjo <seongside@gmail.com>
2021-07-20 22:14:34 +0900

    조회 조건 수정

--- src/main/kotlin/com/pinocchio/santaclothes/apiserver/service/ViewService.kt
+++ src/main/kotlin/com/pinocchio/santaclothes/apiserver/service/ViewService.kt
@@ -46,7 +46,7 @@ class ViewService(
         val user = userService.findByAccessToken(accessToken).orElseThrow()
         val analysisRequest = analysisRequestService.getById(requestId)
         val cloth = analysisRequest.cloth
-        if (analysisRequest.status != AnalysisStatus.DONE) {
+        if (analysisRequest.status != AnalysisStatus.DONE || analysisRequest.status != AnalysisStatus.NOTIFIED) {
             throw NoSuchElementException("$requestId is not exists")
         }
 
